f59bd03b951d83de46d6b381b67ccf8e9016809a
thanhnc1997 <thanhnc1997@gmail.com>
2025-02-04 11:06:57 +0700

    update 2025/02/04

--- js/components/pageFooter.js
+++ js/components/pageFooter.js
@@ -8,7 +8,7 @@ export default async function pageFooter() {
 	template.classList.add('page-footer');
 	template.innerHTML = `
 	<div class="container">
-		<form class="form" action="https://api.web3forms.com/submit" method="POST">
+		<form class="form">
 			<h4>Leave a request and we will contact you as soon as possible</h4>
 			<input type="hidden" name="access_key" value="15af19a1-cb75-40bc-91fc-91e54fcc7a1d">
 			<input type="checkbox" name="botcheck" class="hidden" style="display: none;">
@@ -25,7 +25,7 @@ export default async function pageFooter() {
 			<div class="grid grid-sm-2">
 				<div>
 					<span class="label required">Company name</span>
-					<input class="input" type="text" class="input" name="company_name" placeholder="Your company name" required>
+					<input class="input" type="text" class="input" name="company" placeholder="Your company name" required>
 				</div>
 				<div>
 					<span class="label required">Email</span>
@@ -36,7 +36,7 @@ export default async function pageFooter() {
 				<span class="label">What specialist are you looking for?</span>
 				<input class="input" type="text" class="input" name="note" placeholder="Describe the specialist you need">
 			</div>
-			<button type="submit" class="btn btn-outline">Request a quote</button>
+			<button class="btn btn-outline">Request a quote</button>
 		</form>
 
 		<div class="main-footer grid grid-md-2">
@@ -83,6 +83,43 @@ export default async function pageFooter() {
 		<div>${marqueeText}</div>
 	</div>
 	`;
+	
+	const url = 'https://script.google.com/macros/s/AKfycbwLKtLtJmRyDuo4dNZEd9QlQd-vnA_-8BQfAyOeuUVVJMEEsp41adCEW-bLrv3AZ1Py/exec';
+	
+	template.querySelector('form button').addEventListener('click', async e => {
+		e.preventDefault();
+		let lds = document.createElement('div');
+		lds.classList.add('lds-ring');
+		lds.innerHTML = '<div></div><div></div><div></div>';
+		document.body.classList.add('overflow-hidden');
+		document.body.appendChild(lds);
+		
+		let formData = {
+			name: template.querySelector('input[name="name"]').value,
+			phone: template.querySelector('input[name="phone"]').value,
+			email: template.querySelector('input[name="email"]').value,
+			company: template.querySelector('input[name="company"]').value,
+			note: template.querySelector('input[name="note"]').value,
+		}
+		
+		await send(formData);
+	});
+	
+	async function handleSend() {
+		location.href = '/thanks.html';
+	}
+	
+	async function send(formData) {
+		const response = await fetch(url, {
+			method: 'POST',
+			body: JSON.stringify(formData),
+			headers: { 
+				"Content-Type": "text/plain;charset=utf-8" 
+			}
+		});
+		
+		await handleSend();
+	}
 	
 	return template;
 }
