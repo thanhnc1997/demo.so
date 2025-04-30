3752120f856251372deabf10875860cc7c9c1e83
thanhnc1997 <thanhnc1997@gmail.com>
2025-04-30 18:32:22 +0700

    update 30/04/2025

--- js/components/pageHeader.js
+++ js/components/pageHeader.js
@@ -6,7 +6,7 @@ export default async function pageHeader() {
 	template.classList.add('page-header');
 	template.innerHTML = `
 	<div class="container">
-		<a href="/" class="logo"><img src="../images/logo.svg" alt="Smart Outsourcing"></a>
+		<a href="/demo.so" class="logo"><img src="../images/logo.svg" alt="Smart Outsourcing"></a>
 		<nav>
 			<ul>
 				<li><a ${pathname.includes('about') ? `class="active"` : ''} href="/demo.so/about">About us</a></li>
